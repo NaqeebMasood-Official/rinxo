19501aa55e1a9500cd1164c3c51c059f2e8aff8a
FarooqQaiser <farooqqaiser679@gmail.com>
2025-12-22 09:55:47 +0500

    Server project structure is set

---
+++ server/src/middlewares/logger.middleware.js
@@ -0,0 +1,3 @@
+// Why needed: Track requests and responses for debugging and monitoring purposes.
+
+//When to use: Debugging, auditing requests, and monitoring application performance.
---
+++ server/src/middlewares/role.middleware.js
@@ -0,0 +1,4 @@
+// Why needed: This middleware manages role-based access control by checking user roles before allowing access to certain routes.
+// In simple words, it restricts access to specific parts of the application based on user roles (e.g., admin, user).
+
+// When to use: Admin dashboard routes or any routes that require specific user roles for access.
